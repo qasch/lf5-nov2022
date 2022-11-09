78931859a87346128ba0b3ad10c0452e6cd45643
Kai Schell <kai.schell@signet.de>
2022-11-09 11:30:16 +0100

    addition

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,25 +1,26 @@
 # Auswahl(menu) anzeigen
 # Benutzer nach Rechenoperation fragen
+auswahlmenu = """
+Welche Rechenoperation soll durchgeführt werden?
 
-
-print("""
 [1] addition (+)
 [2] subtraktion (-)
 [3] mulitplikation (*)
 [4] division (/)
-""")
+"""
 
-
-
-print("[1] addition (+)")
-print("[2] subtraktion (-)")
-print("[3] mulitplikation (*)")
-print("[4] division (/)")
-
-
-
+eingabe = input(auswahlmenu)
 
 # Wenn Auswahl richtig
-# Benutzer nach zwei Zahlen fragen
-# Rechenoperation durchführen
+if eingabe == "1":
+    print("addition")
+    # Benutzer nach zwei Zahlen fragen
+    zahl1 = input("Bitte erste Zahl eingeben: ")
+    zahl2 = input("Bitte zweite Zahl eingeben: ")
+    # Rechenoperation durchführen
+    ergebnis = float(zahl1) + float(zahl2)
+    # NOTE: Beste Stelle zur Ausgabe von ergebnis?
+    print(ergebnis)
+else:
+    print("Falsche Eingabe")
 # Ergebnis ausgeben
