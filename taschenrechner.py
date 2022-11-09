78ba187ac12fcd0c5ac36e8cbead44798bebf15e
Kai Schell <kai.schell@signet.de>
2022-11-09 13:47:16 +0100

    Division durch 0

--- taschenrechner.py
+++ taschenrechner.py
@@ -9,6 +9,8 @@ Welche Rechenoperation soll durchgeführt werden?
 [4] division (/)
 """
 
+# NOTE: Division durch 0 ist nicht erlaubt!
+
 eingabe = input(auswahlmenu)
 
 # Wenn Auswahl richtig
@@ -17,8 +19,22 @@ if eingabe == "1":
     # Benutzer nach zwei Zahlen fragen
     zahl1 = input("Bitte erste Zahl eingeben: ")
     zahl2 = input("Bitte zweite Zahl eingeben: ")
+    # TODO: Eingabe prüfen!
     # Rechenoperation durchführen
     ergebnis = float(zahl1) + float(zahl2)
+    # NOTE: Beste Stelle zur Ausgabe von ergebnis?
+    print(ergebnis)
+elif eingabe == "4":
+    print("division")
+    # Benutzer nach zwei Zahlen fragen
+    zahl1 = input("Bitte erste Zahl eingeben: ")
+    zahl2 = input("Bitte zweite Zahl eingeben: ")
+    # TODO: Eingabe prüfen!
+    # Rechenoperation durchführen
+    if zahl2 != "0":
+        ergebnis = float(zahl1) / float(zahl2)
+    else:
+        ergebnis = print("Division durch 0 ist nicht zulässig.")
     # NOTE: Beste Stelle zur Ausgabe von ergebnis?
     print(ergebnis)
 else:
