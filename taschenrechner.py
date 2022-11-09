451a40c1e8d8839350bf9ef1ef024c899155975c
Kai Schell <kai.schell@signet.de>
2022-11-09 11:07:33 +0100

    taschenrechner

---
+++ taschenrechner.py
@@ -0,0 +1,25 @@
+# Auswahl(menu) anzeigen
+# Benutzer nach Rechenoperation fragen
+
+
+print("""
+[1] addition (+)
+[2] subtraktion (-)
+[3] mulitplikation (*)
+[4] division (/)
+""")
+
+
+
+print("[1] addition (+)")
+print("[2] subtraktion (-)")
+print("[3] mulitplikation (*)")
+print("[4] division (/)")
+
+
+
+
+# Wenn Auswahl richtig
+# Benutzer nach zwei Zahlen fragen
+# Rechenoperation durchführen
+# Ergebnis ausgeben
