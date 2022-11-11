b07f26100a91578c726d890c672b4f0cb8d4c3c9
Kai Schell <kai.schell@signet.de>
2022-11-11 09:54:10 +0100

    pseudo coded

---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+# Benutzer wird nach einem Bereich gefragt, z.B. zw. 0 und 15
+# Computer erzeugt eine Zufallszahl in diesem Bereich
+# Merkt sich diese Zahl
+# Benutzer wird aufgefordert, die Zahl zu erraten
+# Computer gibt zurück, ob richtig geraten wurde
+# Oder ob Zahl zu niedrig oder zu hoch war
+# Benutzer kann erneuten Tipp abgeben
+# Spiel ist zu Ende, wenn Zahl erraten wurde
+# kleiner Zusatz: am Schluss wird ausgegeben, wie viele Versuche benötigt wurden
